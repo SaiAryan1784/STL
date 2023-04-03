ceaee28bb0511e71fde12b02e98c05f0bfaa9f94
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-04-04 02:05:21 +0530

    practice

--- Q1.cpp
+++ Q1.cpp
@@ -2,9 +2,31 @@
 #include<vector>
 using namespace std;
 
-void main()
+int main()
 {
     vector<int> vec;
-    
-    
+    vec.push_back(2);
+
+    cout<<"capacity -->"<<vec.capacity()<<endl;
+    cout<<"size -->"<<vec.size()<<endl;
+
+    vec.push_back(3);
+
+    cout<<"capacity -->"<<vec.capacity()<<endl;
+    cout<<"size -->"<<vec.size()<<endl;
+
+    for(int i:vec)
+    {
+        cout<<i<<" ";
+    }cout<<endl;
+
+    vec.pop_back();
+
+    cout<<"capacity -->"<<vec.capacity()<<endl;
+    cout<<"size -->"<<vec.size()<<endl;
+
+    for(int i:vec)
+    {
+        cout<<i<<" ";
+    }cout<<endl;
 }
